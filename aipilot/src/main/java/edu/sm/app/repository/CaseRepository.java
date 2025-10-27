ee0c22b138e2eab3119e758fd9cad21f179357f3
Rediaum <cog030522@naver.com>
2025-10-27 16:55:58 +0900

    Trial 기능 수정

---
+++ aipilot/src/main/java/edu/sm/app/repository/CaseRepository.java
@@ -0,0 +1,35 @@
+package edu.sm.app.repository;
+
+import edu.sm.app.dto.Case;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 사건 Repository (aipilot에서 사용)
+ */
+@Repository
+@Mapper
+public interface CaseRepository {
+
+    /**
+     * 전체 사건 조회
+     */
+    List<Case> selectAll() throws Exception;
+
+    /**
+     * ID로 사건 조회
+     */
+    Case select(int caseId) throws Exception;
+
+    /**
+     * 사건 수정
+     */
+    void update(Case trialCase) throws Exception;
+
+    /**
+     * 상태별 조회
+     */
+    List<Case> selectByStatus(String status) throws Exception;
+}
